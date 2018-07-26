cb537ed5e722e1e9d35739c629a60a39a4b6505b
Unano <to_see_the_world@163.com>
2018-07-26 12:00:57 +0800

    优化无限极联下拉框

--- javascript/draft01/tree/tree.js
+++ javascript/draft01/tree/tree.js
@@ -11,7 +11,9 @@ function Tree (name, data) {
   this.name = name; // 名字
   this.node = document.querySelector('.' + name); // 节点
   this.data = data; // 数据
+  this.chekedData = []; // 选中的选项
   this.mask = null; // 遮罩层
+  this.test = 0;
 }
 Tree.prototype = {
   constructor: Tree,
@@ -30,35 +32,12 @@ Tree.prototype = {
     let i = 0, len = data.length;
     for (i; i < len; i++) {
       let item = data[i];
-      let label = item.label;
       let val = item.value;
+      let label = item.label;
       let li = document.createElement('li');
-      let icon = document.createElement('span');
-      icon.className = 'triangle-icon';
-      let input = document.createElement('input');
-      let text = document.createElement('span');
-      text.textContent = label;  
-      input.type = 'checkbox';
-      input.value = val;
-      let addOption = document.createElement('span');
-      let deleteOption = document.createElement('span');
-      let operations = document.createElement('span');
-      operations.appendChild(addOption);
-      operations.appendChild(deleteOption);
-      operations.className = 'operations';
-      addOption.textContent = '添加';
-      deleteOption.textContent = '删除';
-      let container = document.createElement('div'); // 用来装每行内容的容器
-      container.className = 'option-container';
-      icon.addEventListener('click', this.toggle, false); // 三角图标被点击时
-      text.addEventListener('click', this.toggle, false); // 文本节点被点击时
-      input.addEventListener('change', this.clickCheckBox.bind(null, this), false); // 选择框被点击时
-      addOption.addEventListener('click', this.addOption.bind(null, this), false); // 添加选项
-      deleteOption.addEventListener('click', this.deleteOption, false); // 删除选项
-      container.appendChild(icon);
-      container.appendChild(input);
-      container.appendChild(text);
-      container.appendChild(operations);
+      let option = this.createOption(val, label)
+      let container = option[0];
+      let icon = option[1];
       li.appendChild(container);
       if (item.children && item.children.length > 0) {
         let childrenItems = item.children;
@@ -72,7 +51,6 @@ Tree.prototype = {
 
   // 选择框被点击时，thisObj是当前的tree实例，changedNode是通过事件间接改变了状态的input元素
   clickCheckBox: function (thisObj, changedNode) { 
-    console.log(changedNode.target, '111')
     let that = thisObj;
     let item = null;
     if (changedNode.target) {
@@ -85,8 +63,8 @@ Tree.prototype = {
     } else {
       that.check(item, false);
     }
-    let checkedNodes = that.selectedNodes();
-    console.log(checkedNodes, '选中节点的值');
+    let checkedOptions = that.selectedNodes();
+    this.chekedData = checkedOptions;
   },
   // item参数是被点击的选项，value是子选项需要设置的状态
   check: function (item, value) { 
@@ -127,6 +105,9 @@ Tree.prototype = {
         else if (allNotChecked) {
           parentCheckbox.checked = false;
           this.clickCheckBox(this, parentCheckbox);
+        }
+        else if (!allChecked) {
+          parentCheckbox.checked = false;
         }
       } 
     }
@@ -170,7 +151,7 @@ Tree.prototype = {
         if (item.checked) {
           return item.value;
         } 
-      })
+      });
     }
     let selectedData = selectedItems.filter((item) => {
       return item;
@@ -184,6 +165,8 @@ Tree.prototype = {
     let clearBtn = document.createElement('span');
     saveBtn.textContent = '保存';
     clearBtn.textContent = '清空';
+    saveBtn.addEventListener('click', this.saveData.bind(null, this), false);
+    clearBtn.addEventListener('click', this.clearData.bind(null, this), false);
     btns.className = 'footer';
     saveBtn.className = 'btn';
     clearBtn.className = 'btn';
@@ -192,46 +175,140 @@ Tree.prototype = {
     this.node.appendChild(btns);
   },
   // 设置选项名称的遮罩层
-  addOption: function (thisObj) {
+  addOption: function (thisObj, listBtn) {
     let that = thisObj;
-    if (that.mask) {
-      let mask = that.node.querySelector('.mask');
-      mask.className = 'mask';
-    } else {   
+    let clickedBtn = listBtn;
+    // 展示遮罩层
+    // if (that.mask) {  // 如果将遮罩层存储起来显示隐藏，就不能获取到实时点击的添加按钮
+    //   let mask = that.node.querySelector('.mask');
+    //   mask.className = 'mask';
+    // } else {   
       let mask = document.createElement('div');
       mask.className = 'mask';
       let box = document.createElement('div');
       box.className = 'box';
-      let input = document.createElement('input');
-      input.type = 'text';
-      input.placeholder = '请输入添加的选项名称';
-      input.className = 'input-name';
+      let p1 = document.createElement('p');
+      let label1 = document.createElement('label');
+      label1.textContent = '名称：';
+      let label = document.createElement('input');
+      label.type = 'text';
+      label.placeholder = '请输入添加的选项的名称';
+      label.className = 'input-name';
+      label.id = 'label';
+      p1.appendChild(label1);
+      p1.appendChild(label);
+      let p2 = document.createElement('p');
+      let label2 = document.createElement('label');
+      label2.textContent = '值：';
+      let optionVal = document.createElement('input');
+      optionVal.type = 'text';
+      optionVal.placeholder = '请输入添加的选项的值';
+      optionVal.className = 'input-name';
+      optionVal.id = 'optionVal';
+      p2.appendChild(label2);
+      p2.appendChild(optionVal);
       let footer = document.createElement('div');
       footer.className = 'footer';
       let btn = document.createElement('span');
       btn.textContent = '确定';
       btn.className = 'btn ';
+      btn.id = 'ensure';
+      btn.addEventListener('click', that.submitEdit.bind(null, that, clickedBtn), false);
       let close = document.createElement('span');
       close.textContent = 'X';
       close.className = 'close-mask';
       close.addEventListener('click', that.closeBox.bind(null, that), false);
       footer.appendChild(btn);
       box.appendChild(close);
-      box.appendChild(input);
+      box.appendChild(p1);
+      box.appendChild(p2);
       box.appendChild(footer);
       mask.appendChild(box);
       that.mask = mask;
       that.node.appendChild(mask);
+    // }
+  },
+  // 确定修改内容
+  submitEdit: function (thisObj, btn) {
+    let that = thisObj;
+    let clickedBtn = btn;
+    let li = clickedBtn.parentNode.parentNode.parentNode;
+    let lastChild = li.lastChild;
+    let liNode = document.createElement('li');
+    let optionLabel = that.mask.querySelector('#label').value;
+    let optionVal = that.mask.querySelector('#optionVal').value;
+    let label = optionLabel || 'test';
+    let option = optionVal || 'test';
+    let optionNode = that.createOption(option, label);
+    let contain = optionNode[0];
+    let icon = optionNode[1];
+    contain.removeChild(icon);
+    liNode.appendChild(contain);
+    if (lastChild.nodeName == 'UL') {
+      lastChild.appendChild(liNode);
+    } else {
+      let ulNode = document.createElement('ul');
+      ulNode.appendChild(liNode);
+      li.appendChild(ulNode);
     }
+    that.closeBox(that);
+  },
+  // 删除选项
+  deleteOption: function (thisObj, btn) {
+    // let that = thisObj;
+    let clickedBtn = btn;
+    let li = clickedBtn.parentNode.parentNode.parentNode;
+    li.parentNode.removeChild(li);
+    console.log('删除选项');
   },
   // 关闭设置名称的弹窗时触发
   closeBox: function (thisObj) {
     let that = thisObj;
     that.mask.className += ' mask-hidden';
   },
-  // 删除选项
-  deleteOption: function () {
-    console.log('删除选项');
+  // 创建选项的内容
+  createOption: function (val, label) {
+    let icon = document.createElement('span');
+    icon.className = 'triangle-icon';
+    let input = document.createElement('input');
+    let text = document.createElement('span');
+    text.textContent = label;  
+    input.type = 'checkbox';
+    input.value = val;
+    let addOption = document.createElement('span');
+    let deleteOption = document.createElement('span');
+    let operations = document.createElement('span');
+    operations.appendChild(addOption);
+    operations.appendChild(deleteOption);
+    operations.className = 'operations';
+    addOption.textContent = '添加';
+    deleteOption.textContent = '删除';
+    let container = document.createElement('div'); // 用来装每行内容的容器
+    container.className = 'option-container';
+    icon.addEventListener('click', this.toggle, false); // 三角图标被点击时
+    text.addEventListener('click', this.toggle, false); // 文本节点被点击时
+    input.addEventListener('change', this.clickCheckBox.bind(null, this), false); // 选择框被点击时
+    addOption.addEventListener('click', this.addOption.bind(null, this, addOption), false); // 添加选项
+    deleteOption.addEventListener('click', this.deleteOption.bind(null, this, deleteOption), false); // 删除选项
+    container.appendChild(icon);
+    container.appendChild(input);
+    container.appendChild(text);
+    container.appendChild(operations);
+    return [container, icon];
+  },
+  // 保存选中的内容
+  saveData: function (thisObj) {
+    let that = thisObj;
+    console.log(that.chekedData , '选中的数据')
+  },
+  // 清除数据
+  clearData: function (thisObj) {
+    let that = thisObj;
+    let checkboxs = that.node.querySelectorAll('input[type=checkbox]');
+    Array.prototype.forEach.call(checkboxs, function (item) {
+      item.checked = false;
+    });
+    that.chekedData = [];
   }
 }
 
