0b8cf5f5232a87e70afd48d063881584500a21f5
Unano <to_see_the_world@163.com>
2018-09-29 19:13:00 +0800

    增加内容

---
+++ vue/03vue-ssr/03/practiceVueSSR/layouts/blog.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>这里是博客导航</div>
+    <nuxt />
+  </div>
+</template>
+
+<style scoped>
+div {
+  color: gray;
+}
+</style>
---
+++ vue/03vue-ssr/03/practiceVueSSR/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <img src="~assets/nineteenDays.png" >
+    <img src="~static/nineteenDays.png" >
+  </div>
+</template>
